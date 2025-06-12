8f7566021a596e961d4abb2b6838acc08037bbac
Capen Apple <capen.apple@gmail.com>
2025-06-12 15:34:07 -0500

    Fixed directory

--- test/specs/ios/ios-todo-list.spec.js
+++
@@ -1,9 +0,0 @@
-describe('Todo List', () => {
-  it('Create a Todo List', async () => {
-    await $('//*[@name="Create list"]').click();
-    await $('//*[@value="List Name"]').addValue("Things to do today");
-    await $('~Create').click();
-
-    await expect(await $("~Things to do today")).toBeExisting();
-  });
-});
